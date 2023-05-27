c2aced51fe1bb1dcaeb9737ba88b1686b01041cb
stqp <tokuda.sss@gmail.com>
2023-05-27 23:43:18 +0900

    2.10_ex_1

--- web/modules/custom/hello_world/src/Service/HelloWorldMessenger.php
+++ web/modules/custom/hello_world/src/Service/HelloWorldMessenger.php
@@ -6,6 +6,10 @@ use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Drupal\hello_world\EchoMessageServiceInterface;
 use Drupal\node\NodeInterface;
+use Drupal\core\config\ConfigFactoryInterface;
+use Drupal\core\Extension\ModuleHandlerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
 
 /**
  * A service that echo messages.
@@ -15,12 +19,44 @@ class HelloWorldMessenger implements EchoMessageServiceInterface {
   use StringTranslationTrait;
 
   /**
+   * The messenger service.
+   */
+  protected $configFactory;
+
+  /**
+   * The messenger service.
+   */
+  protected $moduleHandler;
+
+  /**
+   * A construtor of HelloWorldController.
+   *
+   * @param \Drupal\core\config\ConfigFactoryInterface $configFactory
+   *   The messenger service.
+   * @param \Drupal\core\Extension\ModuleHandlerInterface $moduleHandler
+   *   The messenger service.
+   */
+  public function __construct(ConfigFactoryInterface $configFactory, ModuleHandlerInterface $moduleHandler) {
+    $this->configFactory = $configFactory;
+    $this->moduleHandler = $moduleHandler;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('config.factory'),
+      $container->get('module_handler'),
+    );
+  }
+  /**
    * Just say configured hello message.
    *
    * @inheritDoc
    */
   public function helloWorld() {
-    return \Drupal::service('config.factory')->get('hello_world.settings')->get('hello_message');
+    return $this->configFactory->get('hello_world.settings')->get('hello_message');
   }
 
   /**
@@ -38,7 +74,7 @@ class HelloWorldMessenger implements EchoMessageServiceInterface {
    * @inheritDoc
    */
   public function inspectUser(AccountInterface $user) {
-    if (\Drupal::moduleHandler()->moduleExists("devel")) {
+    if ($this->moduleHandler->moduleExists("devel")) {
       dpm($user);
     }
 
