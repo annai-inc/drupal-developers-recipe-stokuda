dda11a7859cbf0072fe2e6b413a3c311a074ad6f
stqp <tokuda.sss@gmail.com>
2023-06-02 02:29:07 +0900

    2.21_ex_2

--- web/modules/custom/hello_world/scripts/update.php
+++ web/modules/custom/hello_world/scripts/update.php
@@ -11,17 +11,31 @@
  * @var \Drupal\Core\Database\Connection $database
  */
 $database = \Drupal::database();
-$result = $database->update('book')
-  ->fields(['description' => 'The awesome book'])
-  ->condition('description', '%' . $database->escapeLike('awesome') . '%', 'LIKE')
-  ->execute();
+$transaction = $database->startTransaction();
+try {
+  $result = $database->update('book')
+    ->fields(['description' => 'The awesome book'])
+    ->condition('description', '%' . $database->escapeLike('awesome') . '%', 'LIKE')
+    ->execute();
 
-var_dump($result);
+  var_dump($result);
 
-$database = \Drupal::database();
-$result = $database->update('book')
-  ->fields(['description' => 'The wonderful book'])
-  ->condition('description', '%' . $database->escapeLike('wonderful') . '%', 'LIKE')
-  ->execute();
+  $raise_exception = $_SERVER['argv'][3];
+  if (isset($raise_exception) && is_string($raise_exception) && $raise_exception == "raise-exception"){
+    throw new Exception("error");
+  }
 
-var_dump($result);
+  $database = \Drupal::database();
+  $result = $database->update('book')
+    ->fields(['description' => 'The wonderful book'])
+    ->condition('description', '%' . $database->escapeLike('wonderful') . '%', 'LIKE')
+    ->execute();
+
+  var_dump($result);
+}
+catch (Exception $e) {
+  $transaction->rollBack();
+}
+
+
+
