959bfbf1c3af254de8ad1b182089730656074283
stqp <tokuda.sss@gmail.com>
2023-06-19 02:15:35 +0900

    3.14.2

--- web/themes/custom/my_awesome_theme/js/message.js
+++ web/themes/custom/my_awesome_theme/js/message.js
@@ -1,5 +1,10 @@
-Drupal.behaviors.welcomeMessage = {
-  attach: function (context, settings) {
-    console.log("Hi user, welcome to my site!");
-  }
-};
+(function($) {
+  Drupal.behaviors.welcomeMessage = {
+    attach: function(context, settings) {
+      $(once('DOMContentLoaded','html')).each(function() {
+        console.log("Hi user, welcome to my site!");
+      });
+    }
+  };
+})(jQuery);
+
